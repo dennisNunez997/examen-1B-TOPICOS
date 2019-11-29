41c302df8b353fc03aa5499a28b7664aa30201a1
dennis <dennis.nunez@epn.edu.ec>
2019-11-29 18:35:25 -0500

    pagina

---
+++ paginaweb/index.js
@@ -0,0 +1,8 @@
+function login()
+{
+    var userEmail = document.getElementById("email_field").nodeValue;
+    var userPass = document.getElementById("password_field").nodeValue;
+
+    windows.alert(userEmail +" "+userPass);
+
+}
